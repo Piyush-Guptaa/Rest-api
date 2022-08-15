0f965627d26627201be9ec81dd41204c127d9ce6
Piyush Gupta <77025176+Piyush-Guptaa@users.noreply.github.com>
2022-08-16 04:16:24 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, redirect
+from flask import jsonify
+import requests
+
+# app = flask.Flask(__name__)
+app = Flask(__name__)
+
+
+@app.route('/hello/', methods=['GET', 'POST'])
+def welcome():
+    return "Hello World!"
+
+
+@app.route('/name')
+def hello():
+    return jsonify({'name': 'Piyush',
+                    'address': 'India'})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=105)
